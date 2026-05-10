a61163b49557b83ebb22667f6b497f653abf21a5
Seleek <148657074+Seleek@users.noreply.github.com>
2026-05-09 22:14:25 -0700

    Mostrar horarios de los alumnos en el script de coordinadores

--- src/components/coordinador.js
+++ src/components/coordinador.js
@@ -32,3 +32,27 @@ function loadHorariosTab() {
         }
     });
 }
+
+function mostrarHorariosAlumno(idAlumno){
+    const inscripcionesAlumno = inscripciones.filter(i => i.id_alumno === idAlumno);
+    const tbody = document.getElementById('horariosTableBody');
+    tbody.innerHTML = '';
+
+    inscripcionesAlumno,forEach(inscripcion => {
+        const grupo = getGrupoById(inscripcion.id_grupo);
+        const materia = getMateriaById(grupo.id_materia);
+        
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td>${materia.nombre}</td>
+        <td>${grupo.grupo}</td>
+        <td>${grupo.semestre}</td>
+        <td><span class = "badge badge-success">${inscripcion.estado}</span></td>
+
+        `;
+        tbody.appendChild(tr);
+    });
+
+    document.getElementById('horariosTable').style.display = 'block';
+}
+
